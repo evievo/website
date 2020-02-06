930755531f81b57ece6ce8df1431f82e76f774b6
evievo <ejvolli@umich.edu>
2020-02-06 01:38:11 -0500

    quick update 2-6-20

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import { TimelineLite, TweenMax, TimelineMax } from "gsap";
+
+//global varibales here:
+let timer = null;
+let hover = false;
 
 class Main extends React.Component {
 
@@ -12,19 +17,50 @@ class Main extends React.Component {
 
   async componentDidMount(){
     try {
-    
 
+      //mouse anim
+      document.addEventListener("mousemove", (e) => {
+        TweenMax.to(this.cursor, 0.6, { css: { left: e.clientX, top: e.clientY, opacity: 0.3 } });
+        TweenMax.to(this.cursor, 0.2, { css: {opacity: 1 } });
+        if (hover){
+          TweenMax.to(this.cursor, 0.6, { css: {opacity: 1, scale: 2} });
+        }
+        else if (!hover){
+          TweenMax.to(this.cursor, 0.6, { css: { scale: 1} });
+        }
+
+      });
+      //mouse animation when clicking
+      document.addEventListener("click", (e) => {
+        TweenMax.to(this.cursor, 0.0, { css: {scale: 1 } });
+        TweenMax.to(this.cursor, 0.4, { css: {scale: 2 } });
+        TweenMax.to(this.cursor, 0.4, { css: {scale: 1 } });
+      });
+
+      let first = {Title: "coasters", Bio: "hey this is design", order: 0}
+      let second = {Title: "clock", Bio: "hey this is also design", order: 1}
+
+      this.setState({
+        projects: [first, second]
+      });
     }
-
     catch(error){
       alert(error);
     }
 
   }
 
+  mouseenter(){
+    hover = true;
+  }
+  mouseleave(){
+    hover = false;
+  }
 
 //////RENDER//////////
   render() {
+
+    const {stuff} = this.props;
 
     return (
       <div>
@@ -33,7 +69,11 @@ class Main extends React.Component {
             <div className="cursor" ref={ cursor => this.cursor = cursor }></div>
 
             <div id="navbar" ref={ navbar => this.navbar = navbar }>
-              <div className="mainNav"><p>evan vollick-offer</p></div>
+              <div className="mainNav"
+              onMouseEnter={ () => this.mouseenter() }
+              onMouseLeave={ () => this.mouseleave() }>
+                <p>evan vollick-offer</p>
+              </div>
               <div className="navNav">
                 <p> <a>work</a> <a>play</a> <a>about</a></p>
 
@@ -42,6 +82,19 @@ class Main extends React.Component {
 
             <div id="page" ref={ page => this.page = page }>
               <div id="projects" ref={ projects => this.projects = projects }>
+
+                { Object.keys(this.state.projects).map((item, i) => {
+                  return(
+                    <div
+                    className = "proj-title"
+                    key = {'Title" + i'}>
+                      {this.state.projects[i]['Title']}
+                    </div>
+
+
+                  )
+                }) }
+
               </div>
             </div>
          </div>
